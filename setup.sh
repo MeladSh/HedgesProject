9dfcca34d915deb55dc068010e259a1b18543917
BoolOrInt <karam.ah3@gmail.com>
2022-07-22 06:40:11 -0700

    comment correction

--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@ install_requirements(){
   sleep 1
   sudo apt-get install python3.8-dev
   sleep 1
-  echo "--------------------Installing numpy1.17.4-------------------"
+  echo "--------------------Installing numpy-------------------"
   sleep 1
   sudo apt-get install python3.8-numpy
 }
 
 run(){
-  # install requirements -> Python3.8 / numpy1.17.4
+  # install requirements -> Python3.8 / numpy
   install_requirements
 }
 
